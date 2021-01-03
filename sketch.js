b00fc1a811ff5b6426b1d0916aecc075a55f8079
harji1234 <harji14@icloud.com>
2021-01-02 21:05:17 -0500

    used database

--- sketch.js
+++ sketch.js
@@ -1,29 +1,44 @@
 var ball;
-
+var database
+var position
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    ball.shapeColor = "red"
+    database=firebase.database();
+    var ref = database.ref('Position')
+    ref.on("value",info)
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        changePosition(-3,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        changePosition(3,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        changePosition(0,-3);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        changePosition(0,+3);
     }
     drawSprites();
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref('Position').set({
+        x : position.x +x,
+        y : position.y +y
+    })
+    
+}
+
+function info(data) {
+position=data.val();
+ball.x = position.x
+ball.y = position.y
+
+
 }
